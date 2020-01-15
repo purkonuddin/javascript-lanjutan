73e5eff49cfeb82ba5dc1bd96c1787dab4f3087e
purkonuddin <purkonud2508@bsi.ac.id>
2020-01-15 10:52:34 +0700

    fungsi Math
    round , floor, ceil

---
+++ math-objectProperty.js
@@ -0,0 +1,8 @@
+console.log(Math.E); //angka logarritma natural e
+console.log(Math.LN10); //angka logarritma natural 10
+console.log(Math.LN2); //angka logarritma natural 2
+console.log(Math.LOG10E); //angka logarritma natural e basis 10
+console.log(Math.LOG2E); //angka logarritma natural e basis 2
+console.log(Math.PI); //berisi angka pi 3.14....
+console.log(Math.SQRT1_2); //angka 1 dibagi dengan akar kuadrat 2
+console.log(Math.SQRT2); //angka kuadrat 2
---
+++ math-pembulatan.js
@@ -0,0 +1,6 @@
+// ceil, floor, and round,
+var x= 12.1;
+
+console.log(Math.round(x));//13 pembulatan
+console.log(Math.ceil(x)); //13 pembulatan ke atas
+console.log(Math.floor(x));//12 pembulatan ke bawah
